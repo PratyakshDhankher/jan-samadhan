3a21910a72e474e12e7278dbb988f52921cccea7
Pratyaksh Dhankher <pratyakshdhankher@gmail.com>
2026-04-25 09:01:15 +0530

    fix: added vercel url in main.py

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,8 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://localhost:3001",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001"
+        "http://127.0.0.1:3001",
+        "https://jan-samadhan-os0j9w49b-pratyakshdhankhers-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
