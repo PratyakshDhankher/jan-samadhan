6bba165e23e5a710733e93afc436225c7e99754a
Pratyaksh Dhankher <pratyakshdhankher@gmail.com>
2026-04-25 10:11:40 +0530

    fix: added the vercel url in main.py

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "https://jan-samadhan-ten.vercel.app",
         "https://jan-samadhan-ten.vercel.app/"
     ],
     allow_credentials=True,
